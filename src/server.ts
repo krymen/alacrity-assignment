3a6a2099da21d31902aa9b04092976d70e57cfa0
Krzysztof Menżyk <krzysztof@menzyk.net>
2019-11-20 23:03:10 +0100

    Add linter

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,9 @@
 import app from './app';
 
 const server = app.listen(app.get('port'), () => {
+  // tslint:disable-next-line: no-console
   console.log('  App is running at http://localhost:%d in %s mode', app.get('port'), app.get('env'));
+  // tslint:disable-next-line: no-console
   console.log('  Press CTRL-C to stop\n');
 });
 
