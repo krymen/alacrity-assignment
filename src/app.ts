5e4af635b11f89513637217210dcf46dbaa3bb61
Krzysztof Menżyk <krzysztof@menzyk.net>
2019-11-21 00:00:18 +0100

    Introduce API for storing value, with validation only

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,23 @@
 import express from 'express';
+import { check, validationResult } from 'express-validator';
+import HttpStatus from 'http-status-codes';
 
 const app = express();
 
 app.set('port', process.env.PORT || 3000);
+app.use(express.json());
 
-app.get('/', (req, res) => res.send('Hello World!'));
+app.put(
+  '/:id',
+  [check('encryption_key').isString(), check('value').isJSON()],
+  (req: express.Request, res: express.Response) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(HttpStatus.BAD_REQUEST).json({ errors: errors.array() });
+    }
+
+    res.status(HttpStatus.NO_CONTENT).send();
+  }
+);
 
 export default app;
--- test/e2e/store.test.ts
+++ test/e2e/store.test.ts
@@ -1,11 +1,58 @@
+import HttpStatus from 'http-status-codes';
 import request from 'supertest';
 import app from '../../src/app';
 
 describe('Encrypted key-value store API', () => {
+  it('stores values', async () => {
+    const response = await request(app)
+      .put('/my-sample-key')
+      .send({
+        encryption_key: 'abc-123',
+        value: JSON.stringify({ a: 1, b: 'string' })
+      });
 
-  it('returns hello world', async () => {
-    const response = await request(app).get('/');
+    expect(response.status).toEqual(HttpStatus.NO_CONTENT);
+  });
 
-    expect(response.text).toEqual('Hello World!');
+  it('returns bad request if encryption key is not a string', async () => {
+    const response = await request(app)
+      .put('/my-sample-key')
+      .send({
+        encryption_key: 1,
+        value: JSON.stringify({ a: 1, b: 'string' })
+      });
+
+    expect(response.status).toEqual(HttpStatus.BAD_REQUEST);
+  });
+
+  it('returns bad request if no encryption key provided', async () => {
+    const response = await request(app)
+      .put('/my-sample-key')
+      .send({
+        value: JSON.stringify({ a: 1, b: 'string' })
+      });
+
+    expect(response.status).toEqual(HttpStatus.BAD_REQUEST);
+  });
+
+  it('returns bad request if value is not of JSON type', async () => {
+    const response = await request(app)
+      .put('/my-sample-key')
+      .send({
+        encryption_key: 'abc-123',
+        value: 'not_json'
+      });
+
+    expect(response.status).toEqual(HttpStatus.BAD_REQUEST);
+  });
+
+  it('returns bad request if no value provided', async () => {
+    const response = await request(app)
+      .put('/my-sample-key')
+      .send({
+        encryption_key: 'abc-123'
+      });
+
+    expect(response.status).toEqual(HttpStatus.BAD_REQUEST);
   });
 });
